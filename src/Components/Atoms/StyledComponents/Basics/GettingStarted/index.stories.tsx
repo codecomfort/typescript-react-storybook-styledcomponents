6bc5073dc7fdab1d79b325b0fbc692970cef6ea7
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2018-09-20 16:40:31 +0900

    [add] styled-components Getting Started

---
+++ src/Components/Atoms/StyledComponents/Basics/GettingStarted/index.stories.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { Title, Wrapper } from "./index";
+
+storiesOf("Atoms/StyledComponents/Basics/GettingStarted", module).add(
+  "default",
+  () => (
+    <Wrapper>
+      <Title>Hello world, this is my first styled component!</Title>
+    </Wrapper>
+  )
+);
